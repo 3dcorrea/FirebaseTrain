b5befe3b18331aed8b5bc9ff1e54407365b4ccdc
Dan Correa <dancorrea3@gmail.com>
2019-04-03 19:15:02 -0400

    HTML and CSS changes

--- assets/Javascript/Javascript.js
+++ assets/Javascript/Javascript.js
@@ -1,13 +1,12 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBkozH79Q48zD8uih-mCz2DOjSWc-zyLDU",
-    authDomain: "train-schedule-5fe74.firebaseapp.com",
-    databaseURL: "https://train-schedule-5fe74.firebaseio.com",
-    projectId: "train-schedule-5fe74",
-    storageBucket: "",
-    messagingSenderId: "341891328107"
+var config = {
+  apiKey: "AIzaSyBkozH79Q48zD8uih-mCz2DOjSWc-zyLDU",
+  authDomain: "train-schedule-5fe74.firebaseapp.com",
+  databaseURL: "https://train-schedule-5fe74.firebaseio.com",
+  projectId: "train-schedule-5fe74",
+  storageBucket: "",
+  messagingSenderId: "341891328107"
 };
 
 firebase.initializeApp(config);
-  
+
 var database = firebase.database();
