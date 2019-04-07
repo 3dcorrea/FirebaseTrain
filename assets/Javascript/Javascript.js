ae458e30eaa5652af10f61e3ddf7b237e467b408
Dan Correa <dancorrea3@gmail.com>
2019-04-06 22:40:35 -0400

    removed some console.logs to be more pro

--- assets/Javascript/Javascript.js
+++ assets/Javascript/Javascript.js
@@ -1,3 +1,5 @@
+// I must have done something wrong with Firebase because I could not get Firebase to sync. I kept getting "null".
+
 var config = {
   apiKey: "AIzaSyBkozH79Q48zD8uih-mCz2DOjSWc-zyLDU",
   authDomain: "train-schedule-5fe74.firebaseapp.com",
@@ -10,7 +12,7 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$('#addTrainBtn').on("click", function() {
+$('#addTrainBtn').on("click", function () {
   // This captures the users input
   var trainName = $("#trainNameInput").val().trim();
   var destination = $("#destinationInput").val().trim();
@@ -18,13 +20,13 @@ $('#addTrainBtn').on("click", function() {
   var frequency = $("#frequencyInput").val().trim();
   // this creates an object that holds train information
   var newTrain = {
-      name: trainName,
-      place: destination,
-      ftrain: firstTrain,
-      freq: frequency
+    name: trainName,
+    place: destination,
+    ftrain: firstTrain,
+    freq: frequency
   }
 
-  // This(v) pushes that(^)  to the database
+  // This(v) pushes that(^) to the database
   database.ref().push(newTrain);
   console.log(newTrain.name);
   // this clears the inputs
@@ -37,8 +39,8 @@ $('#addTrainBtn').on("click", function() {
 });
 
 // This creates a firebase listener for adding trains to the database and dynamically creates a row in the html when the user adds a train
-database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
+database.ref().on("child_added", function (childSnapshot) {
+
   // This stores childSnapshot as a variable
   var trainName = childSnapshot.val().name;
   var destination = childSnapshot.val().place;
@@ -46,16 +48,11 @@ database.ref().on("child_added", function(childSnapshot) {
   var frequency = childSnapshot.val().freq;
   // firstTrain time is pushed back to make sure it comes before current time
   var firstTimeConverted = moment(firstTrain, "HH:mm");
-  console.log(firstTimeConverted);
-  var currentTime = moment().format("HH:mm");
-  console.log("CURRENT TIME: " + currentTime);
+  var currentTime = moment().format("HH:mm");;
   // This stores the difference between the currentTime and first train converted in a variable.
   var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log(firstTrain);
-  console.log("Difference in Time: " + timeDiff);
   // find Remainder of the time left and store in a variable
   var timeRemainder = timeDiff % frequency;
-  console.log(timeRemainder);
   // to calculate minutes till train,we store it in a variable
   var minToTrain = frequency - timeRemainder;
   // this variable is for the next train
