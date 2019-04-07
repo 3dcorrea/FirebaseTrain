a45de816ec166fde88be55a3bcd6a68fb2790732
Dan Correa <dancorrea3@gmail.com>
2019-04-06 22:36:36 -0400

    finishing js

--- assets/Javascript/Javascript.js
+++ assets/Javascript/Javascript.js
@@ -44,5 +44,21 @@ database.ref().on("child_added", function(childSnapshot) {
   var destination = childSnapshot.val().place;
   var firstTrain = childSnapshot.val().ftrain;
   var frequency = childSnapshot.val().freq;
- 
+  // firstTrain time is pushed back to make sure it comes before current time
+  var firstTimeConverted = moment(firstTrain, "HH:mm");
+  console.log(firstTimeConverted);
+  var currentTime = moment().format("HH:mm");
+  console.log("CURRENT TIME: " + currentTime);
+  // This stores the difference between the currentTime and first train converted in a variable.
+  var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log(firstTrain);
+  console.log("Difference in Time: " + timeDiff);
+  // find Remainder of the time left and store in a variable
+  var timeRemainder = timeDiff % frequency;
+  console.log(timeRemainder);
+  // to calculate minutes till train,we store it in a variable
+  var minToTrain = frequency - timeRemainder;
+  // this variable is for the next train
+  var nxTrain = moment().add(minToTrain, "minutes").format("HH:mm");
+  $("#trainTable>tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + nxTrain + "</td><td>" + frequency + "</td><td>" + minToTrain + "</td></tr>");
 });
